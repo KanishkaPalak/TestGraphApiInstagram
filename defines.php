de1fc367e28dcaea68102dfd9967213afc25a741
Kanishka Rao <kanishka.palak@gmail.com>
2020-03-13 18:00:04 +0530

    Update defines.php

--- defines.php
+++ defines.php
@@ -4,7 +4,7 @@
 
 	define( 'FACEBOOK_APP_ID', '214792709896512' );
 	define( 'FACEBOOK_APP_SECRET', '69523abde5ed3e1a00585251e19145b1' );
-	define( 'FACEBOOK_REDIRECT_URI', 'https://socialsizzle.herokuapp.com/auth/' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://kanishkapalak.github.io/TestGraphApiInstagram/obtain_access_token.php/' );
 	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
 	
 
